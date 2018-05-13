a4571315f58ecd7dc2b6b9858a6c0372b38f59fe
Huzaifah Sulaiman <huzaifah_s@Huzaifahs-MacBook-Pro.local>
2018-05-13 12:35:06 -0400

    Created components folder and moved Login and Tabs to views

---
+++ src/components/views/Login/index.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+import LoadTabs from '../Tabs'
+
+class Login extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text>Login</Text>
+                <Button
+                    title="go to home"
+                    onPress={()=>{
+                        LoadTabs();
+                    }}
+                />
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default Login;
