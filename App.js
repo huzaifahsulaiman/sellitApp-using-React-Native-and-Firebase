ad5d683ca046b1225aaead70cddddf4bc59d99f4
Huzaifah Sulaiman <huzaifah_s@Huzaifahs-MacBook-Pro.local>
2018-05-13 21:21:33 -0400

    Lesson 65 - Added Redux

--- App.js
+++ App.js
@@ -1,12 +1,35 @@
 import { Navigation } from 'react-native-navigation';
+import configureStore from './src/components/Store/config';
+import { Provider } from 'react-redux';
 
 import Login from './src/components/views/Login';
 import Home from "./src/components/views/Home";
 import AddPost from "./src/components/views/Admin/AddPost";
 
-Navigation.registerComponent("sellitApp.Login",()=>Login);
-Navigation.registerComponent("sellitApp.Home",()=>Home);
-Navigation.registerComponent("sellitApp.AddPost",()=>AddPost);
+const store = configureStore();
+
+Navigation.registerComponent(
+  "sellitApp.Login",
+  ()=>
+  Login,
+  store,
+  Provider
+);
+
+Navigation.registerComponent(
+  "sellitApp.Home",
+  ()=>
+  Home,
+  store,
+  Provider
+);
+Navigation.registerComponent(
+  "sellitApp.AddPost",
+  ()=>
+  AddPost,
+  store,
+  Provider
+);
 
 export default () => Navigation.startSingleScreenApp({
   screen:{
