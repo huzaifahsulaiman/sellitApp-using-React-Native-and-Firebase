7668054052f32e670cee0705ae3dd9df3189c784
Huzaifah Sulaiman <huzaifah_s@Huzaifahs-MacBook-Pro.local>
2018-05-16 23:04:20 -0400

    Lesson84-85 - Horizontal Scroll

--- src/components/views/Home/index.js
+++ src/components/views/Home/index.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 import { navigatorDrawer, navigatorDeepLink } from '../../utils/misc';
-
+import HorizontalScroll from './horizontal_scroll_icons';
 class Home extends React.Component {
 
     constructor(props){
         super(props);
+
+        this.state = {
+            categories:['All', 'Sports', 'Music', 'Clothing', 'Electronics'],
+            categorySelected:"All"
+        }
 
         this.props.navigator.setOnNavigatorEvent((event)=>{
             navigatorDeepLink(event, this)
@@ -13,15 +18,33 @@ class Home extends React.Component {
         })
     }
 
+    updateCategoryHandler = (value) => (
+        this.setState({
+            categorySelected:value
+        })
+    )
+
     render() {
         return (
-            <Text>Home</Text>
+            <ScrollView>
+                
+                <View style={styles.container}>
+                    <HorizontalScroll
+                        categories={this.state.categories}
+                        categorySelected={this.state.categorySelected}
+                        updateCategoryHandler={this.updateCategoryHandler}
+                    />
+                </View>
+                
+            </ScrollView>
         );
     }
 }
 
 const styles = StyleSheet.create({
-
+    container:{
+        marginTop:5,
+    }
 });
 
 export default Home;
